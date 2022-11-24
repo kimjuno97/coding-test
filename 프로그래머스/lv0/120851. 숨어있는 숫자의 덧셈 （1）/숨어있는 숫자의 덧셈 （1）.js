4ca8b11c5a58e884fcf96d5fe49a2d5b096dd7de
kimjuno97 <jh241093@gmail.com>
2022-11-24 09:46:44 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,6 +1,6 @@
 function solution(my_string) {
     return my_string.split('')
         .filter(x=>{if(!isNaN(x)) return x})
-        .reduce((sum,num)=> sum*1 + num*1, [])
+        .reduce((sum,num)=> sum*1 + num*1, 0)
 
 }

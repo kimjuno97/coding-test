d2ee0de1901d138d3a11181faa27ac6e9be0f69e
kimjuno97 <jh241093@gmail.com>
2022-12-12 16:21:02 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -0,0 +1,22 @@
+function solution(progresses, speeds) {
+  var answer = [];
+  const workday = progresses.map((v, i) => Math.ceil((100 - v) / speeds[i]));
+  
+  let deploy = 1;
+  while (workday.length > 0) {
+    let first = workday[0];
+    if (first < workday[deploy]) {
+      workday.splice(0, deploy);
+      answer.push(deploy);
+      deploy = 1;
+    } else {
+      deploy += 1;
+      if (first === Math.max(...workday)) {
+        answer.push(workday.length);
+        workday.splice(0, workday.length);
+      }
+    }
+  }
+
+  return answer;
+}

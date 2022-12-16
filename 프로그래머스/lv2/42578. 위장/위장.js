da4c4f6e44e3c6541314ec72edba96be227be6f3
kimjuno97 <jh241093@gmail.com>
2022-12-16 12:34:42 +0900

    [level 2] Title: 위장, Time: 0.38 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.js
+++ 프로그래머스/lv2/42578. 위장/위장.js
@@ -1,9 +1,13 @@
 function solution(clothes) {
     let answer = 1;
+    // obj
     const obj = {};
-    clothes.map(clothe => {
-        if (obj[clothe[1]]) return obj[clothe[1]] += 1;
-        return obj[clothe[1]] = 1;
+    clothes.forEach(clothe => {
+        if (obj[clothe[1]]){
+            obj[clothe[1]] += 1
+        } else{
+            obj[clothe[1]] = 1;
+        }
     });
     const keys = Object.keys(obj);
     keys.forEach(key => {

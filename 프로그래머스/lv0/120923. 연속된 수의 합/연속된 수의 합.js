18f9bcebacb0c6249e27371d5c43901ff191a346
kimjuno97 <jh241093@gmail.com>
2023-02-13 08:36:14 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -0,0 +1,8 @@
+function solution(num, total) {
+    let start = Math.floor((total - num * (num - 1) / 2) / num);
+    let result = [];
+    for (let i = 0; i < num; i++) {
+        result.push(start + i);
+    }
+    return result;
+}

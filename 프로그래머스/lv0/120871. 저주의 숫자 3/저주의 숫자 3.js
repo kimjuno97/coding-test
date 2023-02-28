bc28c3212423de07b2ff156c4763d98f00981f57
kimjuno97 <jh241093@gmail.com>
2023-02-28 09:06:23 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,11 +1,12 @@
 function solution(n) {
   const filteredArr = [];
 
-  for (let i = 1; filteredArr.length < n; i++) {
+  for (let i = 1; filteredArr.length < n; i++) { 
+      // 조건에 해당되는 것을 push
     if (i % 3 !== 0 && !i.toString().includes("3")) {
       filteredArr.push(i);
     }
   }
-
+  // 0번부터 시작하니 -1
   return filteredArr[n - 1];
 }

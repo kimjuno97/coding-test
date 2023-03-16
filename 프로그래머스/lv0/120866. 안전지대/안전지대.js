09236f3a653a087c2a3ffb37eaf21ed8872af4de
kimjuno97 <jh241093@gmail.com>
2023-03-17 08:55:02 +0900

    [level 0] Title: 안전지대, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -1,5 +1,5 @@
 function solution(board) {
-    let result = 0
+    let result = 0;
     // 전체 순회
     for(let i = 0 ; i < board.length ; i ++) {
         for(let j = 0 ; j < board[i].length ; j ++) {
@@ -8,39 +8,52 @@ function solution(board) {
                 // 상하 좌우를 2로 변경하되 이는 해당 칸이 0인 경우에만 해당 즉 폭탄은 건들지 않는다.
                 // 맨 윗줄이 아닌 경우
                 if(i !== 0 && board[i-1][j] !== 1) {
-                    board[i-1][j] = 2    
+                    board[i-1][j] = 2;    
                 }
                 // 맨 아랫줄이 아닌 경우
                 if(i !== board.length-1 && board[i+1][j] !== 1) {
-                    board[i+1][j] = 2
+                    board[i+1][j] = 2;
                 }
                 // 맨 왼쪽이 아닌 경우
                 if(j !== 0 && board[i][j-1] !== 1) {
-                    board[i][j-1] = 2
+                    board[i][j-1] = 2;
                 }
                 // 맨 오른쪽이 아닌 경우
                 if(j !== board[i].length-1 && board[i][j+1] !== 1) {
-                    board[i][j+1] = 2
+                    board[i][j+1] = 2;
                 }
                 // 맨 대각선 윗 왼쪽이 아닌 경우
                 if(i !== 0 && j !== 0 && board[i-1][j-1] !== 1) {
-                    board[i-1][j-1] = 2
+                    board[i-1][j-1] = 2;
                 }
                 // 맨 대각선 윗 오른쪽이 아닌 경우
                 if(i !== 0 && j !== board[i].length-1 && board[i-1][j+1] !== 1) {
-                    board[i-1][j+1] = 2
+                    board[i-1][j+1] = 2;
                 }
                 // 맨 대각선 아랫 왼쪽이 아닌 경우
                 if(i !== board.length-1 && j !== 0 && board[i+1][j-1] !== 1) {
-                    board[i+1][j-1] = 2
+                    board[i+1][j-1] = 2;
                 }
                 // 맨 대각선 아랫 오른쪽이 아닌 경우
                 if(i !== board.length-1 && j !== board[i].length-1 && board[i+1][j+1] !== 1) {
-                    board[i+1][j+1] = 2
+                    board[i+1][j+1] = 2;
                 }
             }
         }
     }
-    board.forEach(a => a.forEach(b => b === 0 ? result++ : null))
-    return result
+    board.forEach(a => a.forEach(b => b === 0 ? result++ : null));
+    return result;
 }
+// 다른 사람 풀이.
+function solution(board) {
+    let outside = [[-1,0], [-1,-1], [-1,1], [0,-1],[0,1],[1,0], [1,-1], [1,1]];
+    let safezone = 0;
+
+    board.forEach((row, y, self) => row.forEach((it, x) => {
+        if (it === 1) return false;
+        return outside.some(([oy, ox]) => !!self[oy + y]?.[ox + x])
+               ? false : safezone++;
+    }));
+    return safezone;
+}
+

fda1ea21296f73e4a7b8cff479b5ddcb60d9bd30
kimjuno97 <jh241093@gmail.com>
2023-04-07 10:06:27 +0900

    [unrated] Title: 추억 점수, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -8,3 +8,11 @@ function solution(name, yearning, photo) {
         arr.reduce((acc, curr) => (acc + (obj[curr] ?? 0)),0)
     ))
 }
+
+// 다른 사람 풀이 
+function solution(name, yearning, photo) {
+    return photo.map((v)=> v.reduce((a, c)=> a += yearning[name.indexOf(c)] ?? 0, 0))
+}
+
+// 느낀점. 
+// obj 변수를 선언 할 필요가 없었다.
